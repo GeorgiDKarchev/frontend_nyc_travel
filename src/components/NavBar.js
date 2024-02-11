922d3792dbabe9ab749548f6c8f36128221d1a2a
[Georgi Karchev] <georgikarcev@gmail.com>
2024-02-10 21:41:42 -0500

    Links added to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {useState} from "react"
 import "../index.css"
+import {Link} from "react-router-dom"
 
 //Setting NavBar
 const NavBar =()=>{
@@ -14,9 +15,9 @@ const NavBar =()=>{
             </div>
             <ul className='navMenu'>
                 {/* On every click on menu item, a line will uppear under it */}
-                <li onClick={()=>{setMenu('home')}}>Home{menu==='home'?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu('home')}}>About {menu==='about'?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu('tours')}}>Additional Tours{menu==='tours'?<h/>:<></>}</li>
+                <li onClick={()=>{setMenu('home')}}><Link to ='/'>Home</Link>{menu==='home'?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu('home')}}><Link to ='/About'>About</Link>{menu==='about'?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu('tours')}}><Link to ='/AdditionalTours'>Additional Tours</Link>{menu==='tours'?<h/>:<></>}</li>
             </ul>
             <div className='login'>
                 <img className = 'shopping-cart' src='https://static.vecteezy.com/system/resources/previews/000/356/583/original/vector-shopping-cart-icon.jpg'></img>
